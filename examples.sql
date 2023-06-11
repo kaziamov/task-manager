d8a4d6aaf91698180959d1fe6e7b12af1fdf76c9
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-11 17:04:27 +0300

    Add demo data

---
+++ examples.sql
@@ -0,0 +1,62 @@
+INSERT INTO public.labels_labelmodel ("name") VALUES
+	 ('label_1'),
+	 ('label_2'),
+	 ('label_3'),
+	 ('label_4'),
+	 ('label_5'),
+	 ('label_6'),
+	 ('label_7'),
+	 ('label_8'),
+	 ('label_9');
+
+INSERT INTO public.statuses_statusmodel (name) VALUES
+	 ('status_1'),
+	 ('status_2'),
+	 ('status_3'),
+	 ('status_4'),
+	 ('status_5'),
+	 ('status_6'),
+	 ('status_7'),
+	 ('status_8'),
+	 ('status_9');
+
+INSERT INTO public.auth_user ("password",last_login,is_superuser,username,first_name,last_name,email,is_staff,is_active,date_joined) VALUES
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test0','','','test@test.com',false,true,'2023-06-08 12:45:34.329245+03'),
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test1','','','test1@test.com',false,true,'2023-06-08 12:45:34.329245+03'),
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test2','','','test2@test.com',false,true,'2023-06-08 12:45:34.329245+03'),
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test3','','','test3@test.com',false,true,'2023-06-08 12:45:34.329245+03'),
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test4','','','test4@test.com',false,true,'2023-06-08 12:45:34.329245+03'),
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test5','','','test5@test.com',false,true,'2023-06-08 12:45:34.329245+03'),
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test6','','','test6@test.com',false,true,'2023-06-08 12:45:34.329245+03'),
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test7','','','test7@test.com',false,true,'2023-06-08 12:45:34.329245+03'),
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test8','','','test8@test.com',false,true,'2023-06-08 12:45:34.329245+03'),
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test9','','','test9@test.com',false,true,'2023-06-08 12:45:34.329245+03'),
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test10','','','test10@test.com',false,true,'2023-06-08 12:45:34.329245+03'),
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test11','','','test11@test.com',false,true,'2023-06-08 12:45:34.329245+03'),
+	 ('pbkdf2_sha256$390000$FmESHzjA8AywbsvPK6i6e5$VXvwnALYJkA8GlMRjYiQRd++uYwSbnMqrhZ6gF4g+wk=',NULL,false,'test12','','','test12@test.com',false,true,'2023-06-08 12:45:34.329245+03');
+
+INSERT INTO public.users_usermodel (user_ptr_id,created_at,modified_at) VALUES
+	 (1,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03'),
+	 (2,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03'),
+	 (3,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03'),
+	 (4,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03'),
+	 (5,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03'),
+	 (6,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03'),
+	 (7,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03'),
+	 (8,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03'),
+	 (9,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03'),
+	 (10,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03'),
+	 (11,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03'),
+	 (12,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03'),
+	 (13,'2023-06-08 12:42:50.256811+03','2023-06-08 12:45:34.464096+03');
+
+
+INSERT INTO public.tasks_taskmodel ("name",desk,created_at,updated_at,author_id,executor_id,label_id,status_id) VALUES
+	 ('task_1','some_desc','2023-06-08','2023-06-08',1,1,2,1),
+	 ('task_2','some_desc','2023-06-08','2023-06-08',2,1,2,1),
+	 ('task_3','some_desc','2023-06-08','2023-06-08',3,2,2,1),
+	 ('task_4','some_desc','2023-06-08','2023-06-08',4,5,2,1),
+	 ('task_5','some_desc','2023-06-08','2023-06-08',2,4,2,1),
+	 ('task_6','some_desc','2023-06-08','2023-06-08',2,3,2,1),
+	 ('task_7','some_desc','2023-06-08','2023-06-08',1,2,2,1),
+	 ('task_8','some_desc','2023-06-08','2023-06-08',1,1,2,1);
