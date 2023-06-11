e1eac5b4d2bf585ad3359d5815f6f721455af6e1
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-11 17:02:52 +0300

    Add CRUD views

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from task_manager.views import BaseFormView, BaseListView
+from .models import TaskModel
+from .forms import NewTaskForm
+
+
+class TaskView:
+    model = TaskModel
+
+
+class TasksListView(TaskView, BaseListView):
+    extra_context = {
+        'url': reverse_lazy('task_create'),
+        'create_url': 'task_create',
+        'update_url': 'task_update',
+        'delete_url': 'task_delete'
+    }
+
+
+class TaskCreateView(CreateView, BaseFormView):
+    form_class = NewTaskForm
+    success_url = reverse_lazy('task_create')
+
+
+class TaskUpdateView(TaskView, UpdateView):
+    pass
+
+
+class TaskDeleteView(TaskView, DeleteView):
+    pass
