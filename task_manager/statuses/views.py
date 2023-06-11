0e632b02301a9ffe2740ef58a7f51cc0af929f20
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-11 17:07:22 +0300

    Add CRUD vievs

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from task_manager.views import BaseFormView, BaseListView
+
+from .models import StatusModel
+from .forms import NewStatusForm
+
+
+class StatusView:
+    model = StatusModel
+
+
+class StatusesListView(StatusView, BaseListView):
+    extra_context = {
+        'url': reverse_lazy('status_create'),
+        'create_url': 'status_create',
+        'update_url': 'status_update',
+        'delete_url': 'status_delete'
+    }
+
+
+class StatusCreateView(CreateView, BaseFormView):
+    form_class = NewStatusForm
+    success_url = reverse_lazy('statuses_list')
+
+
+class StatusUpdateView(StatusView, FormView):
+    pass
+
+
+class StatusDeleteView(StatusView, FormView):
+    pass
