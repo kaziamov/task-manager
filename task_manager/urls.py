ce029103496efbb0d2c3c7d319f586922425e480
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-11 17:01:21 +0300

    Update routes

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.views.i18n import set_language
 from django.urls import path, include
-from task_manager.views import HomePageView
+from .views import HomePageView
+from .users.views import LoginView, LogoutView
 
 
 urlpatterns = [
     path('', HomePageView.as_view()),
     path('users/', include('task_manager.users.urls')),
-    path('login/', HomePageView.as_view()),
-    path('logout/', HomePageView.as_view()),
+    path('statuses/', include('task_manager.statuses.urls')),
+    path('tasks/', include('task_manager.tasks.urls')),
+    path('labels/', include('task_manager.labels.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('i18n/', set_language, name='set_language'),
 ]
